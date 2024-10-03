64b02146b01d2fc29b990f3a0921b4ff7bbc6833
Antonio Fortanet <j.a.fortanetcapetillo@student.tudelft.nl>
2024-10-03 23:07:56 +0200

    removed prints and added time stuff

--- riallto.py
+++ riallto.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 def pfb_fir_frontend(x, win_coeffs, M, P, W):
     x_p = x.reshape((W*M, P)).T
@@ -16,30 +17,14 @@ def squaring_pfb(x_pfb):
     return np.real(x_pfb * np.conj(x_pfb)) # same as x_psd = np.abs(x_pfb)**2
 
 def riallto_stuff(x, win_coeffs, M, P, W):
-    print("Input type:")
     x = x[:int(len(x)//(M*P))*M*P] # Ensure it's an integer multiple of win_coeffs
-    print(x.shape)
-    print(len(x))
-    print(type(x))
-
+    
     # Apply frontend, take FFT, then take power (i.e. square)
-    print("\nFIR frontend:")
     x_fir = pfb_fir_frontend(x, win_coeffs, M, P, W)
-    print(x_fir.shape)
-    print(len(x_fir))
-    print(type(x_fir))
 
-    print("\nFFT:")
     x_pfb = fft(x_fir, P) # pfb filterbank
-    print(x_pfb.shape)
-    print(len(x_pfb))
-    print(type(x_pfb))
 
-    print("\nSquare (output):")
     x_psd = squaring_pfb(x_pfb)
-    print(x_psd.shape)
-    print(len(x_psd))
-    print(type(x_psd))
     
 
     return x_psd
@@ -49,7 +34,10 @@ def pfb_spectrometer(x, n_taps, n_chan, n_win, n_int, win_coeffs):
     P = n_chan
     W = n_win
     
+    start = time.time()
     x_psd = riallto_stuff(x, win_coeffs, M, P, W)
+    time_passed = time.time() - start
+    print("Time for Riallto stuff: " + str(time_passed) + "s")
     
     # Trim array so we can do time integration
     x_psd = x_psd[:np.round(x_psd.shape[0]//n_int)*n_int]
